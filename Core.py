6ff0f3a0338138c55b4f42e7df80952ed1bde153
Yumiria <154666142+Yumiara@users.noreply.github.com>
2024-08-23 13:29:29 +0700

    Create Core.py

---
+++ Core.py
@@ -0,0 +1,10 @@
+function removeDashes(a)return a:gsub("-","")end;function reverseString(b)return b:reverse()end;function splitAndReverse(a)local c=#a;local d=math.floor(c/2)local e=a:sub(1,d)local f=a:sub(d+1)local g=reverseString(e)local h=reverseString(f)return g,h end;local i=tostring(game:GetService('RbxAnalyticsService').GetClientId(game:GetService('RbxAnalyticsService')))local j=removeDashes(i)local k,l=splitAndReverse(j)local m=k..l
+list = {
+    "Reversed";
+};
+if table.find(list, m) then if isfile and not isfile("PhoenixKey.bin") and writefile then writefile("PhoenixKey.bin", "PHOENIX-Premium"); end end;
+if table.find(list, m) then while true do end; while task.wait do end; taks.spawn(function()
+    if setfpscap then
+        setfpscap(10);
+    end;
+end); end; if isfile and isfile("PhoenixKey.bin") then while true do end; end;
